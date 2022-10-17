a1a74687c6d421eccbaca785d2b5c76237fffa5c
Damien Regad <dregad@mantisbt.org>
2022-10-17 18:56:34 +0200

    WIP

--- helper.php
+++ helper.php
@@ -20,6 +20,16 @@ class helper_plugin_redirect extends DokuWiki_Admin_Plugin {
     const LEGACY_FILE = __DIR__ . '/redirect.conf';
 
     /**
+     * @var string[] Redirections from config file
+     */
+    protected $redirects;
+
+    /**
+     * @var array Regex-based redirections
+     */
+    protected $regexRedirects = [];
+
+    /**
      * helper_plugin_redirect constructor.
      *
      * handles the legacy file
@@ -39,7 +49,17 @@ class helper_plugin_redirect extends DokuWiki_Admin_Plugin {
      * @return bool
      */
     public function saveConfigFile($config) {
-        return io_saveFile(self::CONFIG_FILE, cleanText($config));
+        $config = cleanText($config);
+
+        // Check regex patterns, report invalid ones
+        $this->redirects = linesToHash(explode("\n", $config));
+        $invalid = $this->checkInvalidPatterns();
+
+        if ($invalid) {
+            msg(sprintf($this->getLang('invalid'), implode('<br>', $invalid)), 2);
+        }
+
+        return io_saveFile(self::CONFIG_FILE, $config);
     }
 
     /**
@@ -61,16 +81,31 @@ class helper_plugin_redirect extends DokuWiki_Admin_Plugin {
      * @return bool|string the full URL to redirect to
      */
     public function getRedirectURL($id) {
-        $redirects = confToHash(self::CONFIG_FILE);
-        if(empty($redirects[$id])) return false;
+        $this->redirects = confToHash(self::CONFIG_FILE);
 
-        if(preg_match('/^https?:\/\//', $redirects[$id])) {
-            $url = $redirects[$id];
+        $redirect = $this->redirects[$id] ?? null;
+
+        // If no "plain" redirection exists, check if we have a match with regex-based ones
+        if (!$redirect) {
+            foreach ($this->getRegexRedirects() as $source => $target) {
+                $newId = preg_replace($source, $target, $id);
+                if ($newId !== null && $newId != $id) {
+                    $redirect = $newId;
+                    break;
+                }
+            }
+        }
+
+        // No redirection found
+        if (!$redirect) return false;
+
+        if(preg_match('/^https?:\/\//', $redirect)) {
+            $url = $redirect;
         } else {
             if($this->getConf('showmsg')) {
                 msg(sprintf($this->getLang('redirected'), hsc($id)));
             }
-            $link = explode('#', $redirects[$id], 2);
+            $link = explode('#', $redirect, 2);
             $url = wl($link[0], '', true, '&');
             if(isset($link[1])) $url .= '#' . rawurlencode($link[1]);
         }
@@ -85,4 +120,79 @@ class helper_plugin_redirect extends DokuWiki_Admin_Plugin {
     {
         return '';
     }
+
+    /**
+     * Check whether the given string is a Regex pattern.
+     *
+     * The function does not actually validate the regex; it only ensures that
+     * the string is bound by valid delimiters
+     * {@see https://www.php.net/manual/en/regexp.reference.delimiters.php},
+     * so it can be processed by pcre_xxx functions.
+     *
+     * @param string $str
+     * @return bool
+     */
+    protected function isRegex($str)
+    {
+        $pattern = '/^(?=([^a-zA-Z0-9\s])) # Starts with a valid delimiter
+            (?|
+                # bracket style: (), {}, [] and <>
+                \(.*\) | \[.*] | \{.*} | <.*>
+                |
+                # other delimiters (back-reference)
+                .+\1
+            )
+            # modifiers
+            [imsxADSUXu]*
+            $/xs';
+
+        return preg_match($pattern, trim($str));
+    }
+
+    protected function getRegexRedirects()
+    {
+        if (!$this->regexRedirects) {
+            foreach ($this->redirects as $source => $target) {
+                if ($this->isRegex($source)) {
+                    $this->regexRedirects[$source] = $target;
+                    unset($this->redirects[$source]);
+                }
+            }
+        }
+
+        return $this->regexRedirects;
+    }
+
+    /**
+     * Check for invalid Regex patterns in the redirections,
+     *
+     * @return string[] Formatted error messages.
+     *
+     * @noinspection PhpUnhandledExceptionInspection PhpDocMissingThrowsInspection
+     */
+    protected function checkInvalidPatterns()
+    {
+        // Error handler to convert PHP warnings from preg_match() to exceptions.
+        set_error_handler(
+            function ($errno, $errstr, $errfile, $errline) {
+                throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+            },
+            E_WARNING
+        );
+
+        // Check regex patterns, log invalid ones including error message
+        $invalid = [];
+        foreach (array_keys($this->getRegexRedirects()) as $source) {
+            try {
+                preg_match($source, '');
+            } catch(Exception $e) {
+                $invalid[] = "<code>$source</code> &ndash; " . $e->getMessage();
+            }
+        }
+
+        restore_error_handler();
+
+        return $invalid;
+    }
+
 }
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,3 +4,5 @@ $lang['name']  = 'Page redirect setup';
 $lang['saved'] = 'Data saved.';
 
 $lang['redirected'] = 'You were redirected from <b>%s</b>.';
+
+$lang['invalid'] = '<strong>Invalid pattern(s) found in configuration !</strong> These will be ignored when processing redirections.<br>%s';
